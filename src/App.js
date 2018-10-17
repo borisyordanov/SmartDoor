107d02a59b4782d6a050a5c0e28583ceff427461
Boris Yordanov <boris.yordanov11@yahoo.com>
2018-10-17 20:30:58 +0200

    Add top menu

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import AccountCircle from '@material-ui/icons/AccountCircle';
+const styles = {
+	root: {
+		flexGrow: 1
+	},
+	toolbar: {
+		justifyContent: 'space-between'
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20
+	}
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		auth: true,
+		anchorEl: null
+	};
+
+	handleChange = event => {
+		this.setState({ auth: event.target.checked });
+	};
+
+	handleMenu = event => {
+		this.setState({ anchorEl: event.currentTarget });
+	};
+
+	handleClose = () => {
+		this.setState({ anchorEl: null });
+	};
+
+	render() {
+		const { classes } = this.props;
+		const { auth, anchorEl } = this.state;
+		const open = Boolean(anchorEl);
+		return (
+			<div className={classes.root}>
+				<AppBar position="absolute" color="default">
+					<Toolbar className={classes.toolbar}>
+						<Typography variant="h6" color="inherit" noWrap>
+							SmartDoor
+						</Typography>
+						{auth && (
+							<div>
+								<IconButton
+									aria-owns={open ? 'menu-appbar' : null}
+									aria-haspopup="true"
+									className={classes.menuButton}
+									onClick={this.handleMenu}
+									color="inherit"
+								>
+									<AccountCircle />
+								</IconButton>
+								<Menu
+									id="menu-appbar"
+									anchorEl={anchorEl}
+									anchorOrigin={{
+										vertical: 'top',
+										horizontal: 'right'
+									}}
+									transformOrigin={{
+										vertical: 'top',
+										horizontal: 'right'
+									}}
+									open={open}
+									onClose={this.handleClose}
+								>
+									<MenuItem onClick={this.handleClose}>
+										Profile
+									</MenuItem>
+									<MenuItem onClick={this.handleClose}>
+										My account
+									</MenuItem>
+								</Menu>
+							</div>
+						)}
+					</Toolbar>
+				</AppBar>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default withStyles(styles)(App);
