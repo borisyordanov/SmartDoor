5505b123c01d6204737e6cadd73aca05453342d5
Boris Yordanov <boris.yordanov11@yahoo.com>
2018-10-21 12:21:24 +0200

    Add: Card list, with stopping/playing/pausing

--- src/App.js
+++ src/App.js
@@ -1,91 +1,151 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import CloseIcon from '@material-ui/icons/Close';
+import Header from './components/Header';
+import ItemCard from './components/ItemCard';
+import AddIcon from '@material-ui/icons/Add';
 
-import AccountCircle from '@material-ui/icons/AccountCircle';
-const styles = {
+const styles = theme => ({
+	close: {
+		padding: theme.spacing.unit / 2
+	},
 	root: {
 		flexGrow: 1
 	},
-	toolbar: {
-		justifyContent: 'space-between'
+	container: {
+		width: '100%',
+		maxWidth: 1400,
+		margin: '30px auto 0 auto'
 	},
-	menuButton: {
-		marginLeft: -12,
-		marginRight: 20
+	paper: {
+		padding: theme.spacing.unit * 2,
+		textAlign: 'center',
+		color: theme.palette.text.secondary
+	},
+	fabAddBtn: {
+		margin: 0,
+		right: 20,
+		bottom: 20,
+		position: 'fixed'
 	}
-};
+});
 
 class App extends Component {
 	state = {
-		auth: true,
-		anchorEl: null
+		showSnackbar: false,
+		snackbarMsg: '',
+		itemList: [
+			{
+				title: 'Test 1',
+				date: 1453766400000,
+				img: 'https://via.placeholder.com/400x225',
+				description:
+					'Lorem ipsum dolor sit amet, id sit fugit oporteat perfecto. Putant ornatus usu cu, munere legimus explicari per no. Eum inani graece similique id, putant perpetua aliquando an eam. Quem solum id pro. Errem consequuntur id his.'
+			},
+			{
+				title: 'Test 2',
+				date: 1485388800000,
+				img: 'https://via.placeholder.com/400x225',
+				description:
+					'Lorem ipsum dolor sit amet, id sit fugit oporteat perfecto. Putant ornatus usu cu, munere legimus explicari per no. Eum inani graece similique id, putant perpetua aliquando an eam. Quem solum id pro. Errem consequuntur id his.'
+			},
+			{
+				title: 'Test 3',
+				date: 1516924800000,
+				img: 'https://via.placeholder.com/400x225',
+				description:
+					'Lorem ipsum dolor sit amet, id sit fugit oporteat perfecto. Putant ornatus usu cu, munere legimus explicari per no. Eum inani graece similique id, putant perpetua aliquando an eam. Quem solum id pro. Errem consequuntur id his.'
+			}
+		]
 	};
 
-	handleChange = event => {
-		this.setState({ auth: event.target.checked });
+	startScan = isUnpaused => {
+		this.setState(state => ({
+			showSnackbar: true,
+			snackbarMsg: isUnpaused ? 'Scan continued' : 'Scan started'
+		}));
 	};
-
-	handleMenu = event => {
-		this.setState({ anchorEl: event.currentTarget });
+	pauseScan = () => {
+		this.setState(state => ({
+			showSnackbar: true,
+			snackbarMsg: 'Scan paused'
+		}));
 	};
+	stopScan = () => {
+		this.setState(state => ({
+			showSnackbar: true,
+			snackbarMsg: 'Scan stopped'
+		}));
+	};
+	handleSnackbarClose = (event, reason) => {
+		if (reason === 'clickaway') {
+			return;
+		}
 
-	handleClose = () => {
-		this.setState({ anchorEl: null });
+		this.setState({ showSnackbar: false, snackbarMsg: '' });
 	};
 
 	render() {
 		const { classes } = this.props;
-		const { auth, anchorEl } = this.state;
-		const open = Boolean(anchorEl);
+		const { itemList, showSnackbar, snackbarMsg } = this.state;
 		return (
 			<div className={classes.root}>
-				<AppBar position="absolute" color="default">
-					<Toolbar className={classes.toolbar}>
-						<Typography variant="h6" color="inherit" noWrap>
-							SmartDoor
-						</Typography>
-						{auth && (
-							<div>
-								<IconButton
-									aria-owns={open ? 'menu-appbar' : null}
-									aria-haspopup="true"
-									className={classes.menuButton}
-									onClick={this.handleMenu}
-									color="inherit"
-								>
-									<AccountCircle />
-								</IconButton>
-								<Menu
-									id="menu-appbar"
-									anchorEl={anchorEl}
-									anchorOrigin={{
-										vertical: 'top',
-										horizontal: 'right'
-									}}
-									transformOrigin={{
-										vertical: 'top',
-										horizontal: 'right'
-									}}
-									open={open}
-									onClose={this.handleClose}
-								>
-									<MenuItem onClick={this.handleClose}>
-										Profile
-									</MenuItem>
-									<MenuItem onClick={this.handleClose}>
-										My account
-									</MenuItem>
-								</Menu>
-							</div>
-						)}
-					</Toolbar>
-				</AppBar>
+				<Header />
+				<div className={classes.container}>
+					<Grid
+						container
+						spacing={24}
+						justify="center"
+						alignItems="center"
+					>
+						{itemList.map((item, i) => (
+							<Grid item key={`item-${i}`}>
+								<ItemCard
+									details={item}
+									startScan={this.startScan}
+									pauseScan={this.pauseScan}
+									stopScan={this.stopScan}
+								/>
+							</Grid>
+						))}
+					</Grid>
+				</div>
+				<Button
+					className={classes.fabAddBtn}
+					variant="fab"
+					color="primary"
+					aria-label="Add"
+				>
+					<AddIcon />
+				</Button>
+				<Snackbar
+					anchorOrigin={{
+						vertical: 'bottom',
+						horizontal: 'right'
+					}}
+					open={showSnackbar}
+					autoHideDuration={6000}
+					onClose={this.handleSnackbarClose}
+					ContentProps={{
+						'aria-describedby': 'message-id'
+					}}
+					message={snackbarMsg}
+					action={[
+						<IconButton
+							key="close"
+							aria-label="Close"
+							color="inherit"
+							className={classes.close}
+							onClick={this.handleSnackbarClose}
+						>
+							<CloseIcon />
+						</IconButton>
+					]}
+				/>
 			</div>
 		);
 	}
