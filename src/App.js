5a4852692cc7c6b6f2902d4849a948bd9778188f
Boris Yordanov <boris.yordanov11@yahoo.com>
2018-10-21 12:52:19 +0200

    Add item functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import CloseIcon from '@material-ui/icons/Close';
 import Header from './components/Header';
 import ItemCard from './components/ItemCard';
 import AddIcon from '@material-ui/icons/Add';
+import Modal from '@material-ui/core/Modal';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const styles = theme => ({
 	close: {
@@ -21,10 +26,15 @@ const styles = theme => ({
 		maxWidth: 1400,
 		margin: '30px auto 0 auto'
 	},
-	paper: {
-		padding: theme.spacing.unit * 2,
-		textAlign: 'center',
-		color: theme.palette.text.secondary
+	modal: {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: theme.spacing.unit * 50,
+		backgroundColor: theme.palette.background.paper,
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing.unit * 4
 	},
 	fabAddBtn: {
 		margin: 0,
@@ -38,6 +48,11 @@ class App extends Component {
 	state = {
 		showSnackbar: false,
 		snackbarMsg: '',
+		showModal: true,
+		newItem: {
+			name: '',
+			description: ''
+		},
 		itemList: [
 			{
 				title: 'Test 1',
@@ -62,6 +77,8 @@ class App extends Component {
 			}
 		]
 	};
+	newItemTitle = React.createRef();
+	newItemDescription = React.createRef();
 
 	startScan = isUnpaused => {
 		this.setState(state => ({
@@ -89,9 +106,27 @@ class App extends Component {
 		this.setState({ showSnackbar: false, snackbarMsg: '' });
 	};
 
+	toggleModal = () => {
+		this.setState(state => ({ showModal: !state.showModal }));
+	};
+
+	saveItem = () => {
+		const itemList = this.state.itemList.slice();
+		itemList.push({
+			title: this.newItemName.current.value,
+			description: this.newItemDescription.current.value,
+			date: Date.now(),
+			img: 'https://via.placeholder.com/400x225'
+		});
+		this.setState({
+			showModal: false,
+			itemList
+		});
+	};
 	render() {
 		const { classes } = this.props;
-		const { itemList, showSnackbar, snackbarMsg } = this.state;
+		const { itemList, showSnackbar, snackbarMsg, showModal } = this.state;
+
 		return (
 			<div className={classes.root}>
 				<Header />
@@ -100,10 +135,9 @@ class App extends Component {
 						container
 						spacing={24}
 						justify="center"
-						alignItems="center"
 					>
 						{itemList.map((item, i) => (
-							<Grid item key={`item-${i}`}>
+							<Grid item xs={6} sm={4} md={3} key={`item-${i}`}>
 								<ItemCard
 									details={item}
 									startScan={this.startScan}
@@ -119,9 +153,59 @@ class App extends Component {
 					variant="fab"
 					color="primary"
 					aria-label="Add"
+					onClick={this.toggleModal}
 				>
 					<AddIcon />
 				</Button>
+				<Modal
+					aria-labelledby="simple-modal-title"
+					aria-describedby="simple-modal-description"
+					open={showModal}
+					onClose={this.toggleModal}
+				>
+					<div className={classes.modal}>
+						<Typography variant="h6" gutterBottom>
+							Add new item
+						</Typography>
+						<Grid container spacing={24}>
+							<Grid item xs={12}>
+								<TextField
+									required
+									id="name"
+									name="name"
+									label="Name"newItemTitle
+									fullWidth
+									autoComplete="name"
+									inputRef={this.newItemName}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									id="description"
+									name="description"
+									label="Description"
+									fullWidth
+									multiline
+									rows={5}
+									autoComplete="description"
+									inputRef={this.newItemDescription}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<Button color="primary" onClick={this.saveItem}>
+									Save
+								</Button>
+								<Button
+									color="secondary"
+									onClick={this.toggleModal}
+								>
+									Cancel
+								</Button>
+							</Grid>
+						</Grid>
+					</div>
+				</Modal>
+
 				<Snackbar
 					anchorOrigin={{
 						vertical: 'bottom',
